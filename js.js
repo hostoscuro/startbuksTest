01073eb749fc200c51693b48857cef822ec59792
vice <vdumeng1985@gmail.com>
2021-05-06 20:47:30 -0400

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+function imgSlider(anything){
+    document.querySelector('.startbucks').src = anything
+
+   
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
