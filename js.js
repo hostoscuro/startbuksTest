0795e44be25cc3f7a59700803473735a0ad8cc51
vice <vdumeng1985@gmail.com>
2021-05-28 19:52:54 -0400

    update media query

--- js.js
+++ js.js
@@ -7,4 +7,14 @@ function imgSlider(anything){
 function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
+
+}
+
+function toggleMenu() {
+         
+    var menuToggle= document.querySelector('.toggle');
+    var navigation=document.querySelector('.navigation');
+    menuToggle.classList.toggle('encendido');
+    navigation.classList.toggle('encendido');
+
 }
